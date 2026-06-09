832b9440c3601ea115797c641646a102e9b958ac
jitesh2804 <117830473+jitesh2804@users.noreply.github.com>
2026-06-09 14:20:20 +0530

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -26,11 +26,10 @@ try:
     query = f"""
     SELECT
         CASE
-            WHEN LENGTH(COALESCE(crm.ticketid::text, c.uniqueid::text)) = 10
-                 AND COALESCE(crm.ticketid::text, c.uniqueid::text) ~ '^[0-9]{10,12}$'
-            THEN ''
-            ELSE COALESCE(crm.ticketid::text, c.uniqueid::text)
-        END AS ticketId,
+    WHEN COALESCE(crm.ticketid::text, c.uniqueid::text) ~ '^[0-9]{10,}$'
+    THEN ''
+    ELSE COALESCE(crm.ticketid::text, c.uniqueid::text)
+    END AS ticketId
 
         '{current_date}' AS ftpPath,
         r.recfilename AS fileName,

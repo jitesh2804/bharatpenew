a554963d886dc7cd14eb0149e836139a9c8da9b8
jitesh2804 <117830473+jitesh2804@users.noreply.github.com>
2026-08-18 09:32:02 +0530

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -21,7 +21,7 @@ db_params = {
 # Format: YYYYMMDD
 # ============================================================
 
-REPORT_DATE = "20260816"
+REPORT_DATE = "20260811"
 
 # Convert report date
 report_date = datetime.strptime(REPORT_DATE, "%Y%m%d")
